20cd5d3fe1b388d4cbdf28599beb7d32b6a4589d
Sow-coding <sow.coding@gmail.com>
2023-03-29 00:21:15 +0200

    95% of the page without responsive

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+const imgUn = document.querySelector("#img-un")
+const imgDeux = document.querySelector("#img-deux")
+const btnsEffect1 = document.querySelector(".btnsEffect1")
+const btnsEffect2 = document.querySelector(".btnsEffect2")
+const overeffect1 = document.querySelector(".overeffect1")
+const overeffect2 = document.querySelector(".overeffect2")
 
+
+overeffect1.addEventListener("mouseover", () => {
+    imgUn.classList.add("over")
+    btnsEffect1.classList.add("btnsDisplayOn")
+    imgUn.addEventListener("mouseout", () => {
+        imgUn.classList.remove("over")
+        btnsEffect1.classList.remove("btnsDisplayOn")
+    })
+})
+
+
+overeffect2.addEventListener("mouseover", () => {
+    imgDeux.classList.add("over")
+    btnsEffect2.classList.add("btnsDisplayOn")
+    imgDeux.addEventListener("mouseout", () => {
+        imgDeux.classList.remove("over")
+        btnsEffect2.classList.remove("btnsDisplayOn")
+    })
+})
